ebee600dfc28edb713d337888ff611203149cf50
Hemant Bansal <hem.ban.061@gmail.com>
2016-10-24 22:24:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hemantbansal/subtract/SimpleSubtraction.java
@@ -0,0 +1,17 @@
+package com.example.hemantbansal.subtract;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleSubtraction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_subtraction);
+
+
+    }
+}
